d17b74ecc10d5f4fc0a41eef6e92c047dd98b862
MustafaElSayed <mustafa01148990430@gmail.com>
2026-04-02 16:23:53 +0200

    feat: implement Student class with fields, constructor, and grade management

--- src/GradeCalculator.java
+++ src/GradeCalculator.java
@@ -1,2 +1,5 @@
+// TODO: implement calculateAverage(HashMap<String, Integer> grades)
+// TODO: implement getLetterGrade(double average)
+// TODO: implement calculateClassAverage(ArrayList<Student> students)
 public class GradeCalculator {
 }
