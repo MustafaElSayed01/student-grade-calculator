ad33cabc2e883f946acce7a621098dcbb6f9bd8c
MustafaElSayed <mustafa01148990430@gmail.com>
2026-04-03 16:43:59 +0200

    feat: add addStudent and addGrade methods to Main

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import java.util.Scanner;
 
 public class Main {
+    private static ArrayList<Student> students = new ArrayList<>();
 
     public static void main(String[] args) {
 
@@ -29,5 +30,62 @@ public class Main {
         return result.toString().trim();
     }
 
+    static void addStudent(Scanner scanner) {
+        System.out.println("Please Enter Student ID:");
+        String idInput = scanner.nextLine();
+        try {
+            int id = Integer.parseInt(idInput);
+            if (id <= 0) {
+                System.out.println("ID must be a positive number.");
+                return;
+            }
+            boolean isExists = students.stream().anyMatch(student -> student.getId() == id);
+            if (isExists) {
+                System.out.println("Student with ID " + id + " already exists.");
+                return;
+            }
+            System.out.println("Please Enter Student Name:");
+            String name = scanner.nextLine();
+            String capitalizedName = capitalizeWords(name.trim());
+            Student student = new Student(id, capitalizedName);
+            students.add(student);
+        } catch (NumberFormatException e) {
+            System.out.println("Not a number! Please enter a valid id.");
+        }
+    }
+
+    static void addGrade(Scanner scanner) {
+        System.out.println("Please Enter Student ID:");
+        String idInput = scanner.nextLine();
+        try {
+            int id = Integer.parseInt(idInput);
+            if (id <= 0) {
+                System.out.println("ID must be a positive number.");
+                return;
+            }
+            Optional<Student> found = students.stream().filter(s -> s.getId() == id).findFirst();
+            if (found.isEmpty()) {
+                System.out.println("No student with ID " + id);
+                return;
+            }
+            Student student = found.get();
+
+            System.out.println("Please Enter Class Name");
+            String subject = scanner.nextLine();
+            String capitalizedSubject = capitalizeWords(subject.trim());
+            System.out.println("Please Enter Grade:");
+            String grade = scanner.nextLine();
+            try {
+                double gradeValue = Double.parseDouble(grade);
+                student.addGrade(capitalizedSubject, gradeValue);
+            } catch (NumberFormatException e) {
+                System.out.println("Not a number! Please enter a valid grade.");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Not a number! Please enter a valid one.");
+        }
+    }
 
 }
